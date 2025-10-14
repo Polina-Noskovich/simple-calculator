e2a001ffc5577ffba6cc3d0b0ed33c64a6441032
Polina <noskovicpolina@gmail.com>
2025-10-14 22:03:26 +0300

    feat(build): Configure Webpack for optimized production build (2 files)

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        mode: isProduction ? 'production' : 'development',
+        
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            compress: true,
+            port: 9000,
+            open: true,
+            hot: true,
+        },
+        
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                inject: 'body', 
+            }),
+        ],
+        
+        optimization: {
+            minimize: isProduction,
+        },
+    };
+};
